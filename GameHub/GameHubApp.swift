d4c4256b0a0493e69b68ea7e14ea04c5a65744ea
ShenSudara <sudarakainfo2000@gmail.com>
2026-07-01 16:33:25 +0530

    project initialization

---
+++ GameHub/GameHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameHubApp.swift
+//  GameHub
+//
+//  Created by Ashen Sudaraka on 2026-07-01.
+//
+
+import SwiftUI
+
+@main
+struct GameHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
